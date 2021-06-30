3037973ebdcf82cb9c69960547163dde0e296c48
nhAsif <49162687+nhAsif@users.noreply.github.com>
2021-06-30 11:11:42 +0600

    Cherish os rosy official-rebuild fix some issue

--- build_rom.sh
+++ build_rom.sh
@@ -3,6 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/CherishOS/android_manif
 git clone https://github.com/nhAsif/local_manifest.git --depth 1 -b cherish .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
+
 # build rom
 source build/envsetup.sh
 lunch cherish_rosy-userdebug
