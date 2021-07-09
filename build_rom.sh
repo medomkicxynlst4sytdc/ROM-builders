3ca015f4af1933f3d0ab0be7b4c7dc9fc7b40ea7
nhAsif <49162687+nhAsif@users.noreply.github.com>
2021-07-09 14:18:28 +0600

    fix some bug & rebuild official

--- build_rom.sh
+++ build_rom.sh
@@ -6,9 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch cherish_rosy-userdebug
 export TZ=Asia/Dhaka #put before last build command
-export CHERISH_NONGAPPS=true
+lunch cherish_rosy-userdebug
 mka bacon -j$(nproc --all)
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
