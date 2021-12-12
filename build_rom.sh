a9d51f4f20a67ab114ba6ced3966d9930daeb921
sajidshahriar72543 <sazidshahriar39@gmail.com>
2021-12-12 19:16:26 +0600

    [Bery] Initial Styx S

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,16 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/ArcaneOS/Arcane_manifest -b R -g default,-mips,-darwin,-notdefault
-git clone https://github.com/sajidshahriar72543/local_manifest.git --depth 1 -b main .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/StyxProject/manifest -b S -g default,-mips,-darwin,-notdefault
+git clone https://github.com/sajidshahriar72543/local_manifest.git --depth 1 -b styx .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #14
 
 # build rom
 . build/envsetup.sh
-lunch aosp_beryllium-userdebug
+lunch styx_beryllium-userdebug
 export TZ=Asia/Dhaka #put before last build command
 export BUILD_HOSTNAME=6e6f6f62
-mka bacon
+m styx-ota
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
