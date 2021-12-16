70113dfa5a458532622541399c3a23ce132d84cb
sajidshahriar <sazidshahriar39@gmail.com>
2021-12-16 12:23:43 +0600

    [Bery] Initial Styx S

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/StyxProject/manifest -b
 git clone https://github.com/sajidshahriar72543/local_manifest.git --depth 1 -b styx .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#24
+#25
 
 # build rom
 . build/envsetup.sh
