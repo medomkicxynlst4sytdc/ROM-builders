8d2db11f1773c7a1c658a9c46864f44787a1aa4b
sajidshahriar72543 <sazidshahriar39@gmail.com>
2021-12-19 21:07:41 +0600

    [Bery] Initial Syberia 12

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,17 @@
 # sync rom
-repo init --depth=1 --no-repo-verify -u git://github.com/StyxProject/manifest -b S -g default,-mips,-darwin,-notdefault
-git clone https://github.com/sajidshahriar72543/local_manifest.git --depth 1 -b styx .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u git://github.com/syberia-project/manifest.git -b 12.0 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/sajidshahriar72543/local_manifest.git --depth 1 -b syb .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 #23
 
 # build rom
-. build/envsetup.sh
-lunch styx_beryllium-userdebug
+source build/envsetup.sh
+export SELINUX_NEVERALLOWS=true
+lunch syberia_beryllium-userdebug
 export TZ=Asia/Dhaka #put before last build command
 export BUILD_HOSTNAME=6e6f6f62
-m styx-ota
+make bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
