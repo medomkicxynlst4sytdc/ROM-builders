815bbfeecb72801b188060553e1248c555f00f1f
Rahul Pal XDA <RahulPal.XDA@gmail.com>
2021-12-28 21:46:28 +0530

    Set CUSTOM_BUILD_TYPE to Testing

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch aosp_tulip-userdebug
+export CUSTOM_BUILD_TYPE=Testing
 export TZ=Asia/Kolkata #put before last build command
 mka bacon
 
