d3ba47e1dc454cf1a102ff78479df3f036e0df19
sajidshahriar <sazidshahriar39@gmail.com>
2021-12-31 15:15:45 +0600

    [bery] Initial SSOS S

--- build_rom.sh
+++ build_rom.sh
@@ -3,7 +3,7 @@ repo init --depth=1 --no-repo-verify -u git://github.com/ShapeShiftOS/android_ma
 git clone https://github.com/sajidshahriar72543/local_manifest.git --depth 1 -b ssos .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-#26
+#27
 
 # build rom
 source build/envsetup.sh
