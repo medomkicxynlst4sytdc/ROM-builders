a203cbf485c8c0801a5461dcd27e0468a0e5cb27
Asif <49162687+nhAsif@users.noreply.github.com>
2022-01-17 15:57:55 +0600

    rosy: make 2.3

--- build_rom.sh
+++ build_rom.sh
@@ -6,13 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch awaken_rosy-userdebug
-export ALLOW_MISSING_DEPENDENCIES=true
-export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
-export SELINUX_IGNORE_NEVERALLOWS=true
-export BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES=true
 export TZ=Asia/Dhaka
 make bacon
-
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
