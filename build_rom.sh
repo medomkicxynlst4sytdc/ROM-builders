165d6eaef4b8bc6c7648b3f9c81ef6504446b955
Rahul Pal XDA <RahulPal.XDA@gmail.com>
2022-01-18 17:45:28 +0530

    Initial Build

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,13 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/LineageOS/android.git -b lineage-18.1 -g default,-mips,-darwin,-notdefault
-git clone https://github.com/RahulPalXDA/local_manifest.git --depth 1 -b lineage_twolip .repo/local_manifests
+git clone https://github.com/RahulPalXDA/local_manifest.git --depth 1 -b lineage_j2y18lte .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
-lunch lineage_twolip-userdebug
+lunch lineage_j2y18lte-userdebug
 export TZ=Asia/Kolkata #put before last build command
-brunch twolip
+brunch j2y18lte
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
