a7995a725e7a3f5e6cc597d1d235ab7b0192721d
laleeroy <gabwielcastwo@gmail.com>
2022-02-14 09:01:53 +0800

    Build with neverallows flag
    Signed-off-by: laleeroy <gabwielcastwo@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch radiant_alioth-userdebug
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Manila #put before last build command
 mka bacon 
 
