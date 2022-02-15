bfb6c4af7051670017fb03cb728a77bb4e3ab703
laleeroy <gabwielcastwo@gmail.com>
2022-02-15 13:37:27 +0800

    User build
    Signed-off-by: laleeroy <gabwielcastwo@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -5,8 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch radiant_alioth-userdebug
-export SELINUX_IGNORE_NEVERALLOWS=true
+lunch radiant_alioth-user
 export TZ=Asia/Manila #put before last build command
 mka bacon 
 
