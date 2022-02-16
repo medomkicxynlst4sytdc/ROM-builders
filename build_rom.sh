89c29db2992555192c8beb6f352cee8ad4f0abf4
laleeroy <gabwielcastwo@gmail.com>
2022-02-16 08:09:35 +0800

    Fixed partition sizing
    Signed-off-by: laleeroy <gabwielcastwo@gmail.com>

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-lunch radiant_alioth-user
+lunch radiant_alioth-userdebug
 export TZ=Asia/Manila #put before last build command
 mka bacon 
 
