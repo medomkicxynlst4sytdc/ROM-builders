a323cf1fd0ebe22854d49e1e5192a280cf0d5256
MukulSharma <87255315+MukulSharma06175@users.noreply.github.com>
2022-02-18 19:15:23 +0530

    Let's make blaster for lavender

--- build_rom.sh
+++ build_rom.sh
@@ -4,7 +4,7 @@ git clone https://github.com/MukulSharma06175/Local-Manifests --depth 1 -b blast
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
-source build/envsetup.sh
+. build/envsetup.sh
 lunch aosp_lavender-eng
 export TZ=Asia/kolkata #put before last build command
 make bacon
