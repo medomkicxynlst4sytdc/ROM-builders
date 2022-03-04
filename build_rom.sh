ccc5c09f2c394aa0cc86da9d7372b12fd02932dc
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2022-03-04 21:52:05 +0530

    Set  BUILD_BROKEN_MISSING_REQUIRED_MODULES

--- build_rom.sh
+++ build_rom.sh
@@ -8,6 +8,7 @@ source build/envsetup.sh
 lunch spark_mi439-userdebug
 export WITH_GAPPS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
+export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
 
