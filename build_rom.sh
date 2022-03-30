2e7b4a01ee6a3d918573604143d70bc3c341b553
AzureHelper <92246586+AzureHelper@users.noreply.github.com>
2022-03-30 23:30:49 +0530

    Fix sync error
    * Updated manifest

--- build_rom.sh
+++ build_rom.sh
@@ -1,16 +1,15 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u https://github.com/AOSPA/manifest -b sapphire -g default,-mips,-darwin,-notdefault
-git clone https://github.com/AzureHelper/local_manifests.git --depth 1 -b AOSPA .repo/local_manifests
+git clone https://github.com/AzureHelper/local_manifests.git --depth 1 -b aospa-wip .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
-# build rom [6]
+# build rom [7]
 source build/envsetup.sh
 lunch aospa_raphael-userdebug
 export ALLOW_MISSING_DEPENDENCIES=true
 export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
-ln -sf vendor/aospa/build.sh build.sh
-./build.sh raphael
+./rom-build.sh raphael
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
 rclone copy out/target/product/$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1)/*.zip cirrus:$(grep unch $CIRRUS_WORKING_DIR/build_rom.sh -m 1 | cut -d ' ' -f 2 | cut -d _ -f 2 | cut -d - -f 1) -P
