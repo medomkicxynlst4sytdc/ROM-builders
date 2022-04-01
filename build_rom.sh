bb60d9f28d65ab5c5e8c12708a9d6aeb5a14b776
AzureHelper <92246586+AzureHelper@users.noreply.github.com>
2022-04-01 07:42:01 +0530

    Fix never allow failure

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom [11]
 source build/envsetup.sh
 lunch aospa_raphael-userdebug
-export SELINUX_IGNORE_NEVERALLOWS := true
+export ALLOW_MISSING_DEPENDENCIES=true
+export SELINUX_IGNORE_NEVERALLOWS=true
 export TZ=Asia/Dhaka #put before last build command
 ./rom-build.sh raphael
 
