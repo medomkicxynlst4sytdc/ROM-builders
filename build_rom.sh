4e62f966fe78a02dd91c8e75d73dc57adcb8109b
Hitesoni | ×͜× <72127977+hsx02@users.noreply.github.com>
2022-04-21 12:45:13 +0530

    build spark gapps

--- build_rom.sh
+++ build_rom.sh
@@ -1,13 +1,14 @@
 # sync rom
 repo init --depth=1 --no-repo-verify -u git://github.com/Spark-Rom/manifest.git -b spark -g default,-mips,-darwin,-notdefault
-git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b spark-12 .repo/local_manifests
+git clone https://github.com/hsx02/Local-Manifests.git --depth 1 -b spark-12.1 .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
 source build/envsetup.sh
 lunch spark_mi439-userdebug
-export WITH_GAPPS=true
 export SELINUX_IGNORE_NEVERALLOWS=true
+export WITH_GAPPS=true
+export TARGET_FACE_UNLOCK_SUPPORTED=false
 export BUILD_BROKEN_MISSING_REQUIRED_MODULES=true
 export TZ=Asia/Dhaka #put before last build command
 mka bacon
