14d2e931429d28d032a12d1db07a95f6d568b03d
Asif <49162687+nhAsif@users.noreply.github.com>
2022-05-20 08:11:42 +0600

    alioth: trigger build

--- build_rom.sh
+++ build_rom.sh
@@ -7,6 +7,8 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 source build/envsetup.sh
 lunch lineage_alioth-userdebug
 export TZ=Asia/Dhaka
+export BUILD_USERNAME=NHA
+export BUILD_HOSTNAME=nhAsif
 m bacon
 
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
