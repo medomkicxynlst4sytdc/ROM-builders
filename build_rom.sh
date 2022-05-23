46f31ffb54ca03b19f8daa48ccb3fea5d5dc623a
Rahul Pal XDA <RahulPal.XDA@gmail.com>
2022-05-23 05:32:31 +0530

    allow BUILD_BROKEN_DUP_RULES

--- build_rom.sh
+++ build_rom.sh
@@ -6,6 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_ginkgo-userdebug
+export BUILD_BROKEN_DUP_RULES=true
 export TZ=Asia/Kolkata #put before last build command
 make bacon
  
