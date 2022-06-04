5b2470e85436537c4529830b274cc0e704260ac7
T.B.S <ewprjkt@proton.me>
2022-06-05 02:09:51 +0700

    recommit trees

--- build_rom.sh
+++ build_rom.sh
@@ -6,8 +6,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_X00TD-userdebug
-export BUILD_USERNAME=morbid
-export BUILD_BROKEN_DUP_RULES=true
+export BUILD_USERNAME=wizard
 export TZ=Asia/Jayapura #put before last build command
 mka bacon
 
