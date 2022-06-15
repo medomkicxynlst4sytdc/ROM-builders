e210e7e2629adfbd7342db4e13ec26fd19d5c136
T.B.S <ewprjkt@proton.me>
2022-06-15 22:02:15 +0700

    Resync build

--- build_rom.sh
+++ build_rom.sh
@@ -6,7 +6,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch lineage_X00TD-userdebug
-export BUILD_USERNAME=wizard
 export TZ=Asia/Jayapura #put before last build command
 mka bacon
 
