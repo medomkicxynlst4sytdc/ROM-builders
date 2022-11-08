a65c28726faed6a726ac5524714a7fba4aeb34ae
Tejas Singh <tejassingh649@gmail.com>
2022-11-08 16:37:25 +0530

    Arrow:Vanilla

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,6 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 # build rom
 source build/envsetup.sh
 lunch arrow_ginkgo-userdebug
-export ARROW_GAPPS=true
 export TZ=Asia/Delhi #put before last build command
 m bacon
 # upload rom (if you don't need to upload multiple files, then you don't need to edit next line)
