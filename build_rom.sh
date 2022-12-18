1cc6191735a3fe674d274f7e8c350975b26e6c3a
swapnil vicky <74442290+SwapnilVicky@users.noreply.github.com>
2022-12-18 16:20:24 +0530

    :)

--- build_rom.sh
+++ build_rom.sh
@@ -5,7 +5,7 @@ repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync 
 
 # build rom
 source build/envsetup.sh
-device_xiaomi_miatoll
+lunch device_xiaomi_miatoll
 export TZ=Asia/Kolkata #put before last build command
 make bacon
 
